2d194446559bb34082779151721ab07802759727
QuasarDev20 <dimarek96@gmail.com>
2016-10-14 01:01:49 +0300

    Update!
    Now using JSON + default timer = 15sec + settings .

--- Parser app/Parser app/Form1.Designer.cs
+++ Parser app/Parser app/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -67,7 +72,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Location = new System.Drawing.Point(91, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(10, 13);
             this.label2.TabIndex = 3;
@@ -85,9 +90,9 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(94, 51);
+            this.textBox2.Location = new System.Drawing.Point(159, 51);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
             this.textBox2.TabIndex = 5;
             // 
             // label3
@@ -95,15 +100,70 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.Size = new System.Drawing.Size(141, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Frequency(sec)";
+            this.label3.Text = "Frequency(15sec by default)";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "HC Stats";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(94, 104);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "REG Stats";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(176, 104);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(70, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Nemeses";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Visible = false;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(298, 2);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(57, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Save?";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Settings : not found";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(392, 108);
+            this.ClientSize = new System.Drawing.Size(392, 133);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -125,10 +185,15 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         public System.Windows.Forms.Label label1;
-        protected System.Windows.Forms.Label label2;
         public System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Parser app/Parser app/Form1.cs
+++ Parser app/Parser app/Form1.cs
@@ -1,102 +1,201 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
-using System.Text.RegularExpressions;
 using System.IO;
 using System.Windows.Forms;
-using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        public Stats stats {
+            get
+            {  
+                WebClient web = new WebClient();
+                Stream str = web.OpenRead(textBox1.Text);
+                StreamReader sr = new StreamReader(str);
+                string srr = sr.ReadLine();
+                return(JsonConvert.DeserializeObject<Stats>(srr));
+            }
+            set { } }
         public Form1()
         {
-            InitializeComponent();        
+            InitializeComponent();
+            load();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int cycle = int.Parse(textBox2.Text) * 1000;
+            int timer=0; int.TryParse(textBox1.Text, out timer);
+            long cycle = 15000 +  timer * 1000;
             System.Threading.Timer tmr = new System.Threading.Timer(Parse, null , 1000 , cycle);
-            
-
+            print();           
         }
         public void Parse(object data)    // Парсинг данных в файл + вызов print()
         {
-            Action act = print;
-            WebClient web = new WebClient();
-            Stream str = web.OpenRead(textBox1.Text);
-            StreamReader sr = new StreamReader(str);
-            string srr = sr.ReadLine();
-            Regex hctest = new Regex(@"(?<=""hardcore_summary"")+(:\w*)");
-            Regex regK = new Regex(@"(?<=""kills"")+(:\w*)");
-            Regex regW = new Regex(@"(?<=""wins"")+(:\w*)");
-            Regex regG = new Regex(@"(?<=""global_rank"")+(:\w*)");
-            Match resK, resW, resG;
-            Match hctestm = hctest.Match(srr);          
-            int reg = srr.IndexOf("regular_summary", 0);
-            int rege = srr.IndexOf("}", reg);
-            string sreg = srr.Substring(reg, rege - reg);
-            regK = new Regex(@"(?<=""kills"")+(:\w*)");
-            resK = regK.Match(sreg);
-            regW = new Regex(@"(?<=""wins"")+(:\w*)");
-            resW = regW.Match(sreg);
-            regG = new Regex(@"(?<=""global_rank"")+(:\w*)");
-            resG = regG.Match(sreg);
-            FileStream kills = new FileStream(@"kills.txt", FileMode.Create);
-            StreamWriter writer = new StreamWriter(kills);
-            writer.Write(resK.ToString());
-            writer.Close();
-            FileStream wins = new FileStream(@"wins.txt", FileMode.Create);
-            writer = new StreamWriter(wins);
-            writer.Write(resW.ToString());
-            writer.Close();
-            FileStream global = new FileStream(@"global.txt", FileMode.Create);
-            writer = new StreamWriter(global);
-            writer.Write(resG.ToString());
-            writer.Close();
-            if (hctestm.ToString() != ":null")
+            try
             {
-                int hc = srr.IndexOf("hardcore_summary", 0);
-                int hce = srr.IndexOf("}", hc);
-                string shc = srr.Substring(hc, hce - hc);
-                regK = new Regex(@"(?<=""kills"")+(:\w*)");
-                resK = regK.Match(shc);
-                regW = new Regex(@"(?<=""wins"")+(:\w*)");
-                resW = regW.Match(shc);
-                regG = new Regex(@"(?<=""global_rank"")+(:\w*)");
-                resG = regG.Match(shc);
-                kills = new FileStream(@"killshc.txt", FileMode.Create);
-                writer = new StreamWriter(kills);
-                writer.Write(resK.ToString());
-                writer.Close();
-                wins = new FileStream(@"winshc.txt", FileMode.Create);
-                writer = new StreamWriter(wins);
-                writer.Write(resW.ToString());
-                writer.Close();
-                global = new FileStream(@"globalhc.txt", FileMode.Create);
-                writer = new StreamWriter(global);
-                writer.Write(resG.ToString());
-                writer.Close();
+                if (checkBox2.Checked)
+                {
+                    FileStream kills = new FileStream(@"kills.txt", FileMode.Create);
+                    StreamWriter writer = new StreamWriter(kills);
+                    writer.Write(stats.regular_summary.kills);
+                    writer.Close();
+                    FileStream wins = new FileStream(@"wins.txt", FileMode.Create);
+                    writer = new StreamWriter(wins);
+                    writer.Write(stats.regular_summary.wins);
+                    writer.Close();
+                    FileStream global = new FileStream(@"global.txt", FileMode.Create);
+                    writer = new StreamWriter(global);
+                    writer.Write(stats.regular_summary.global_rank);
+                    writer.Close();
+                }
+                if (checkBox1.Checked)
+                {
+                    FileStream kills = new FileStream(@"killshc.txt", FileMode.Create);
+                    StreamWriter writer = new StreamWriter(kills);
+                    writer.Write(stats.hardcore_summary.kills);
+                    writer.Close();
+                    FileStream wins = new FileStream(@"winshc.txt", FileMode.Create);
+                    writer = new StreamWriter(wins);
+                    writer.Write(stats.hardcore_summary.wins);
+                    writer.Close();
+                    FileStream global = new FileStream(@"globalhc.txt", FileMode.Create);
+                    writer = new StreamWriter(global);
+                    writer.Write(stats.hardcore_summary.global_rank);
+                    writer.Close();
+                }
+                if (checkBox3.Checked)
+                {
+                    FileStream nemeses = new FileStream(@"nemeses.txt", FileMode.Create);
+                    StreamWriter writer = new StreamWriter(nemeses);
+                    writer.Write(stats.nemeses[0].times_died);
+                    writer.Close();
+                }
+                if (checkBox4.Checked)
+                {
+                    save();
+                }             
             }
-            Invoke(act);
+            catch { }
         }
-        private void print() // Пишет слеши в лэйбл с каждым обновлением статы
+        private void print() // Пишет ник из статы
         {
-            label2.Text += " | ";
+            label2.Text =stats.name;
         }
+        private void save() // Сохраняет файл настроек
+        {
+            File.Delete(@"settings.txt");
+            FileStream settings = new FileStream(@"settings.txt", FileMode.Create);
+            StreamWriter writer = new StreamWriter(settings);
+            writer.WriteLine(textBox1.Text);
+            writer.Close();
+        }
+        private string load() // Загрузка ссылки из файла настроек
+        {
+            if (File.Exists(@"settings.txt"))
+            {
+                FileStream settings = new FileStream(@"settings.txt",FileMode.Open);
+                StreamReader reader = new StreamReader(settings);
+                textBox1.Text = reader.ReadLine();
+                label4.Text = "Settings : found !";
+                print();
+            }
+            return (textBox1.Text);
 
+        }
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+        public class Rank
+        {
+            public string rank { get; set; }
+            public string color { get; set; }
+        }
+
+        public class HardcoreSummary
+        {
+            public int kills { get; set; }
+            public int wins { get; set; }
+            public int losses { get; set; }
+            public string time_played { get; set; }
+            public double win_points { get; set; }
+            public double kill_points { get; set; }
+            public string distance_moved { get; set; }
+            public int global_rank { get; set; }
+            public double total_points { get; set; }
+            public Rank rank { get; set; }
+            public int average_placement { get; set; }
+        }
+        public class RegularSummary
+        {
+            public int kills { get; set; }
+            public int wins { get; set; }
+            public int losses { get; set; }
+            public string time_played { get; set; }
+            public double win_points { get; set; }
+            public double kill_points { get; set; }
+            public string distance_moved { get; set; }
+            public int global_rank { get; set; }
+            public double total_points { get; set; }
+            public Rank rank { get; set; }
+            public int average_placement { get; set; }
+        }
+
+        public class LongestKill
+        {
+            public string name { get; set; }
+            public string profile_url { get; set; }
+            public double distance { get; set; }
+            public string weapon { get; set; }
+            public string match_id { get; set; }
+            public string match_url { get; set; }
+        }
+
+        public class Victim
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string profile_url { get; set; }
+            public int times_killed { get; set; }
+        }
+
+        public class Nemes
+        {
+            public string id { get; set; }
+            public string name { get; set; }
+            public string profile_url { get; set; }
+            public int times_died { get; set; }
+        }
+
+        public class TopGun
+        {
+            public string name { get; set; }
+            public int kills { get; set; }
+        }
+
+        public class Map
+        {
+            public string map { get; set; }
+            public int times_played { get; set; }
+            public double percentage { get; set; }
+        }
+
+        public class Stats
+        {
+            public string name { get; set; }
+            public string uid { get; set; }
+            public string profile_url { get; set; }
+            public HardcoreSummary hardcore_summary { get; set; }
+            public RegularSummary regular_summary { get; set; }
+            public List<LongestKill> longest_kills { get; set; }
+            public List<Victim> victims { get; set; }
+            public List<Nemes> nemeses { get; set; }
+            public List<TopGun> top_guns { get; set; }
+            public List<Map> maps { get; set; }
+        }
     }
 }
+
